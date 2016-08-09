39fcc6384d652da86a0a6fc5696a186cd742ea4b
herman <=yermanmicha@gmail.com>
2016-08-08 20:37:19 -0500

    actualizacion

---
+++ holamundo.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# print "hola mundo"
+
+# In[ ]:
+
+
+
+
+# ∫∞xdtt(t2−1)log(t)
+# 
+
+# ∫∞xdtt(t2−1)log(t)
+
+# $$∫∞xdtt(t2−1)log(t)$$
+
+# In[11]:
+
+2+4*6/8.0-3
+
+
+# # hola mundo de python
+
+# In[14]:
+
+3/2
+
+
+# In[15]:
+
+3.0/2.0
+
+
+# In[16]:
+
+3+5j
+
+
+# In[17]:
+
+3+5j-1-1j
+
+
+# In[18]:
+
+abs(3+5j)
+
+
+# In[20]:
+
+5**3
+
+
+# In[21]:
+
+int(_20)
+
+
+# In[22]:
+
+float(_21)
+
+
+# In[25]:
+
+complex(_21)
+
+
+# In[27]:
+
+round(_18)
+
+
+# In[28]:
+
+max(8,10,20,30)
+
+
+# In[31]:
+
+num=10
+
+
+# In[32]:
+
+print num
+
+
+# In[33]:
+
+x,y=a,z
+
+
+# In[35]:
+
+a,b,c,d=1,2,3,4
+
+
+# In[36]:
+
+b
+
+
+# In[43]:
+
+x=5
+y=8
+if x>y:
+    print"el mayor es",x
+else:
+    print"el mayor es",y
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
